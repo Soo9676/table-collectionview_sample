d3412b28edac277ce721720a3d50150d298c9f04
shjeongtde <shjeong@bluedigm.com>
2022-02-14 18:17:16 +0900

    스토리보드 및 뷰컨트롤러 생성

---
+++ sample/sample/ArrayDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  ArrayDataModel.swift
+//  sample
+//
+//  Created by soo on 2022/02/14.
+//
+
+import Foundation
+
+open class ArrayData{
+    public init() {}
+    
+    let array = [("a","b"), ("c","d")]
+}
---
+++ sample/sample/CollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewController.swift
+//  sample
+//
+//  Created by soo on 2022/02/14.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
